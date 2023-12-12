73c562af7c6daf7c40c0a0c009e659606f10a9da
Caio Machado <caiomachadodesouza1@gmail.com>
2023-12-12 11:14:29 -0300

    Remove codigo não mais utilizado

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
-use std::net::TcpListener;
-
-mod connection;
+mod server;
 
 fn main() {
-    let addres: &str = "127.0.0.1:4221";
-    match TcpListener::bind(addres) {
-        Ok(listener) => {
-            for connection in listener.incoming() {
-                match connection {
-                    Ok(mut client_to_handle) => {
-                        connection::handle_client(&mut client_to_handle);
-                    }
-                    Err(e) => {
-                        println!("Connection failure in {e:?}");
-                    }
-                }
-            }
-        },
-        Err(error) => {
-            println!("An error occurred when trying to bind addres: {}", addres);
-            println!("Error: {}", error);
-        },
-    }
+    server::run("127.0.0.1:4221");
 }
